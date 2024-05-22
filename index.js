cf50abf69b7b0aaa148c5543c39240fc4f034f19
Oxana Chizitcaia <creativesouloxana@gmail.com>
2024-05-22 21:24:42 +0200

    adding

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const formular = document.querySelector('form')
+const formular = document.querySelector('form');
 
-const odebirat = (event) => {
-	event.preventDefault()
-	const input = document.querySelector('input')
-	const email = input.value
-	formular.textContent = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${email}.`
-}
+const newsTake = (event) => {
+  event.preventDefault();
+  const input = document.querySelector('input');
+  const email = input.value;
+  formular.textContent = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${email}.`;
+};
 
-formular.addEventListener('submit', odebirat)
+formular.addEventListener('submit', newsTake);
